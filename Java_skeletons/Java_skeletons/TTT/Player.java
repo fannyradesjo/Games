41ed21ca3477a27d89c124de8199ea71c4c4edd2
fanny <fantastiskafanny@gmail.com>
2018-09-27 14:26:39 +0200

    sådärså

--- Java_skeletons/Java_skeletons/TTT/Player.java
+++ Java_skeletons/Java_skeletons/TTT/Player.java
@@ -84,7 +84,7 @@ public class Player {
 
        for(int i = 0; i < 2*size+2; i++){
          if(score[i] != null){
-           score_sum++;
+           score_sum = score_sum + score[i];
          }
        }
 
