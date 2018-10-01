f03b16cee72bf3b2e1bd7ac8e5c99f80a519513f
fannyradesjo <fantastiskafanny@gmail.com>
2018-10-01 09:04:42 +0200

    D:

--- Java_skeletons/Java_skeletons/TTT/Player.java
+++ Java_skeletons/Java_skeletons/TTT/Player.java
@@ -14,6 +14,26 @@ public class Player {
      * @return the next state the board is in after our move
      */
 
+     public static Integer fixScore(Integer score,int cell,int player){
+       if(cell == player){
+         if(score == null){score = 1;}
+         if(score > 0){
+           score = score*10;
+         }
+         if(score < 0){score = 0;}
+     }
+     if(cell == 3-player){
+       if(score == null){score = -1;}
+       if(score < 0){
+         score = score*10;
+       }
+       if(score > 0){score = 0;}
+     }
+
+     return score;
+     }
+
+
      public static int evaluate(int player, GameState state){
        //points for each marker in a row you are alone in
        int size = state.BOARD_SIZE;
@@ -25,75 +45,40 @@ public class Player {
        for(int row = 0; row < size; row++){
          for(int col = 0; col < size; col++){
            cell = state.at(row,col);
-           if(cell == player){
-             if(score[row] == null){ score[row] = 1;}
-               else{
-             if(score[row] > 0){score[row]++;}
-             if(score[row] < 0){score[row] = 0;}
-           }
-           if(score[size+col] == null){ score[size+col] = 1;}
-             else{
-             if(score[size+col] > 0){score[size+col]++;}
-             if(score[size+col] < 0){score[size+col] = 0;}
-           }
-             if(row == col){
-               if(score[2*size] == null){ score[2*size] = 1;}
-                 else{
-                   if(score[2*size] > 0){score[2*size]++;}
-                   if(score[2*size] < 0){score[2*size] = 0;}
-                 }
-             }
-             if(row+col == size-1){
-               if(score[2*size+1] == null){ score[2*size+1] = 1;}
-                 else{
-                   if(score[2*size+1] > 0){score[2*size+1]++;}
-                   if(score[2*size+1] < 0){score[2*size+1] = 0;}
-                 }
-             }
+
+           if(cell != 0){
+           score[row] = fixScore(score[row], cell, player);
+           score[size+col] = fixScore(score[size+col],cell, player);
+
+           if(row == col){
+             score[2*size] = fixScore(score[2*size], cell, player);
            }
 
-           if(cell == opponent){
-             if(score[row] == null){ score[row] = -1;}
-               else{
-             if(score[row] < 0){score[row]--;}
-             if(score[row] > 0){score[row] = 0;}
-           }
-           if(score[size+col] == null){ score[size+col] = -1;}
-             else{
-             if(score[size+col] < 0){score[size+col]--;}
-             if(score[size+col] > 0){score[size+col] = 0;}
-           }
-             if(row == col){
-               if(score[2*size] == null){ score[2*size] = -1;}
-                 else{
-                   if(score[2*size] < 0){score[2*size]--;}
-                   if(score[2*size] > 0){score[2*size] = 0;}
-                 }
-             }
-             if(row+col == size-1){
-               if(score[2*size+1] == null){ score[2*size+1] = -1;}
-                 else{
-                   if(score[2*size+1] < 0){score[2*size+1]--;}
-                   if(score[2*size+1] > 0){score[2*size+1] = 0;}
-                 }
-             }
-             }
+           if(row + col == size){
+             score[2*size+1]=fixScore(score[2*size+1],cell, player);
            }
          }
+
+         }
+       }
 
 
        for(int i = 0; i < 2*size+2; i++){
          if(score[i] != null){
-           score_sum = score_sum + score[i];
+           score_sum++;
          }
        }
 
        return score_sum;
      }
 
-     public static int minmax(GameState state, int depth, int player){
+     public static int minmax(GameState state, int depth, int player, Deadline deadline){
        int bestPossible;
        GameState maxState = new GameState();
+
+       if(deadline.timeUntil() < 100000){
+         return 0;
+       }
 
        Vector<GameState> possibleStates = new Vector<GameState>();
        state.findPossibleMoves(possibleStates);
@@ -105,7 +90,7 @@ public class Player {
        if(player == 1){
          bestPossible = -Integer.MAX_VALUE;
          for(GameState s: possibleStates){
-           int v = minmax(s,depth-1, 2);
+           int v = minmax(s,depth-1, 2, deadline);
            //bestPossible = java.lang.Math.max(bestPossible, v);
            if (v>=bestPossible) {
             bestPossible = v;
@@ -119,7 +104,7 @@ public class Player {
        else{
          bestPossible = Integer.MAX_VALUE;
          for(GameState s: possibleStates){
-           int v = minmax(s,depth-1, 1);
+           int v = minmax(s,depth-1, 1, deadline);
            //bestPossible = java.lang.Math.min(bestPossible, v);
            if (v<bestPossible) {
             bestPossible = v;
@@ -131,39 +116,60 @@ public class Player {
        }
      }
 
-     public static int alphabeta(GameState state,int depth,int alpha,int beta,int player){
-       int v;
+     public static int alphabeta(GameState state,int depth,int alpha,int beta,int player, Deadline deadline){
+            int v;
+            int bestPossible;
+            GameState maxState = new GameState();
 
-       Vector<GameState> possibleStates = new Vector<GameState>();
-       state.findPossibleMoves(possibleStates); //varför ej beroende av player????????????
+            if(deadline.timeUntil() < 10000000){
+              System.err.println("dedaline!!!!!!");
+              return 0;
+            }
 
-       if(depth == 0 || possibleStates.size() == 0) {
-         v = evaluate(player, state);
-       }
-       else if(player == 1) {
-         v = -Integer.MAX_VALUE;
-         for(GameState s: possibleStates){
-           v = java.lang.Math.max(v, alphabeta(s,depth-1,alpha,beta,2));
-           alpha = java.lang.Math.max(alpha, v);
-           if(beta <= alpha){
-             break;
-           }
-         }
-       }
-       else {
-         v = Integer.MAX_VALUE;
-         for(GameState s: possibleStates){
-           v = java.lang.Math.min(v, alphabeta(s, depth-1, alpha,beta,1));
-           beta = java.lang.Math.min(beta, v);
-           if(beta <= alpha){
-             break;
-           }
-         }
-       }
-       return v;
-     }
+            Vector<GameState> possibleStates = new Vector<GameState>();
+            state.findPossibleMoves(possibleStates); //varför ej beroende av player????????????
+
+            if(depth == 0 || possibleStates.size() == 0) {
+              bestPossible = evaluate(player, state);
+            }
+            else if(player == 1) {
+              bestPossible = -Integer.MAX_VALUE;
+              for(GameState s: possibleStates){
+                v = alphabeta(s,depth-1,alpha,beta,2, deadline);
+                alpha = java.lang.Math.max(alpha, v);
+                if(v > bestPossible){
+                  bestPossible = v;
+                  maxState  = s;
+                }
+                if(beta <= alpha){
+                  break;
+                }
+              }
+            }
+            else {
+              bestPossible = Integer.MAX_VALUE;
+              for(GameState s: possibleStates){
+                v = alphabeta(s, depth-1, alpha,beta,1,deadline);
+                beta = java.lang.Math.min(beta, v);
+                if (v < bestPossible) {
+                 bestPossible = v;
+                 maxState  = s;
+                 }
+                if(beta <= alpha){
+                  break;
+                }
+              }
+            }
+            nextBestState = maxState;
+            return bestPossible;
+          }
+
 
     public GameState play(final GameState gameState, final Deadline deadline) {
+      int depth = 4;
+      int alpha = -Integer.MAX_VALUE;
+      int beta = Integer.MAX_VALUE;
+
         Vector<GameState> nextStates = new Vector<GameState>();
         gameState.findPossibleMoves(nextStates);
 
@@ -172,7 +178,7 @@ public class Player {
             return new GameState(gameState, new Move());
         }
 
-        int v = minmax(gameState, 3, 1);
+        int v = alphabeta(gameState, depth, alpha, beta, 1, deadline);
 
         /**
          * Here you should write your algorithms to get the best next move, i.e.
@@ -182,4 +188,5 @@ public class Player {
         //return nextStates.elementAt(random.nextInt(nextStates.size()));
         return nextBestState;
     }
+
 }
